c001cb274c0049a23818862da03b08faae54caa9
Cedric Chee <cedric@invictusbyte.com>
2021-01-24 16:59:16 +0800

    Add test cases

--- tests/tensor.js
+++ tests/tensor.js
@@ -9,11 +9,30 @@ describe('tch-js', function () {
     expect([...ones.toUint8Array()]).to.deep.equal([1, 1, 1, 1, 1, 1]);
   });
 
-  it('tensor', function () {
-    const arr = new Float32Array([1.1, 2.0, 3.0, 4.1]);
-    const tensor = torch.tensor(arr);
+  describe('tensor', function () {
+    it('toFloat32Array', function () {
+      const arr = new Float32Array([
+        1.100000023841858,
+        1.590000033378601,
+        2.049999952316284,
+        0.18000000715255737,
+      ]);
+      const tensor = torch.tensor(arr);
 
-    expect([...tensor.toUint8Array()]).to.deep.equal([1, 2, 3, 4]);
+      expect([...tensor.toFloat32Array()]).to.deep.equal([
+        1.100000023841858,
+        1.590000033378601,
+        2.049999952316284,
+        0.18000000715255737,
+      ]);
+    });
+
+    it('toUint8Array', function () {
+      const arr = new Float32Array([1.1, 2.0, 3.0, 4.1]);
+      const tensor = torch.tensor(arr);
+
+      expect([...tensor.toUint8Array()]).to.deep.equal([1, 2, 3, 4]);
+    });
   });
 
   it('load', function (done) {
